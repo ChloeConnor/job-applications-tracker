d9c9a91f468650905737bd0c2132fdd5d9f9593b
chloeconnor <chloe.connor@vortexa.com>
2020-04-18 13:42:12 +0100

    different form

--- screens/AddApplicationScreen.js
+++ screens/AddApplicationScreen.js
@@ -1,59 +1,58 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import t from "tcomb-form-native";
 import Colors from "../constants/Colors";
+import { TextInput } from "react-native";
 
-const Form = t.form.Form;
-
-const Status = t.enums({
+const Status = {
   applied: "Applied",
   phoneInterview: "Phone interview",
   second_interview: "Second Interview",
   final_interview: "Final Interview",
   take_home_test: "Take home test",
   offer: "Offer",
-});
-
-const Application = t.struct({
-  company: t.String,
-  role: t.String,
-  salary: t.Integer,
-  company_size: t.Integer,
-  location: t.String,
-  applied_via: t.String,
-  status: Status,
-  notes: t.String,
-});
-
-const options = {
-  fields: {
-    status: {
-      options: [
-        { value: "applied", text: "Applied" },
-        { value: "phoneInterview", text: "Phone interview" },
-        { value: "second_interview", text: "Second Interview" },
-        { value: "final_interview", text: "Final Interview" },
-        { value: "take_home_test", text: "Take home test" },
-        { value: "offer", text: "Offer" },
-      ],
-    },
-  },
+  rejected: "Rejected",
+  cancelled: "Cancelled",
 };
 
-export default function AddApplicationScreen() {
+const FormWithSubmit = () => {
+  const [formValues, setFormValues] = useState({});
+
+  function onSubmit() {
+    console.log(formValues);
+  }
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
     >
       <View style={styles.container}>
-        <Form type={Application} options={options} />
-      </View>
-      <View>
+        <TextInput
+          onChangeText={(value) =>
+            setFormValues({ company: value, ...formValues })
+          }
+        >
+          Company
+        </TextInput>
+        <TextInput
+          onChangeText={(value) =>
+            setFormValues({ role: value, ...formValues })
+          }
+        >
+          Role
+        </TextInput>
+        <TextInput
+          onChangeText={(value) => {
+            console.log(value)
+            setFormValues({ salary: value, ...formValues })
+          }}
+        >
+          Salary
+        </TextInput>
         <TouchableHighlight
           style={styles.button}
-          onPress={onPress}
+          onPress={onSubmit}
           underlayColor="#99d9f4"
         >
           <Text style={styles.buttonText}>Save</Text>
@@ -63,12 +62,11 @@ export default function AddApplicationScreen() {
       <Text></Text>
     </ScrollView>
   );
-}
-
-const onPress = async (values) => {
-  await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
 };
+
+export default function AddApplicationScreen() {
+  return <FormWithSubmit></FormWithSubmit>;
+}
 
 const styles = StyleSheet.create({
   container: {
