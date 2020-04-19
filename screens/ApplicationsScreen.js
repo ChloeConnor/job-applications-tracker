250c838f2ac8d826fcb7bb253817457f37493037
chloeconnor <chloe.connor@vortexa.com>
2020-04-19 10:18:02 +0100

    delete applications

--- screens/ApplicationsScreen.js
+++ screens/ApplicationsScreen.js
@@ -7,39 +7,42 @@ import { AsyncStorage } from "react-native";
 import Colors from "../constants/Colors";
 
 function deleteApplication(jobID) {
-  console.log("delete")
-  console.log(jobID)
-  // AsyncStorage.removeItem(jobID);
+  console.log("delete");
+  console.log(jobID);
+  AsyncStorage.removeItem(jobID);
 }
 
 function applicationCards(applications) {
-  return Object.entries(applications).map(([key, value]) => {
-    console.log(value);
-    const app = Object.entries(JSON.parse(value[1]));
-    const id = value[0];
+  
+  if (applications != undefined && applications.length) {
+    return Object.entries(applications).map(([key, value]) => {
+      console.log(value);
+      const app = Object.entries(JSON.parse(value[1]));
+      const jobID = value[0];
 
-    const company = app.filter(([key, value]) => key == "company")[0][1];
-    const role = app.filter(([key, value]) => key == "role")[0][1];
-    const salary = app.filter(([key, value]) => key == "salary")[0][1];
+      const company = app.filter(([key, value]) => key == "company")[0][1];
+      const role = app.filter(([key, value]) => key == "role")[0][1];
+      const salary = app.filter(([key, value]) => key == "salary")[0][1];
 
-    return (
-      <Card
-        containerStyle={styles.card}
-        titleStyle={styles.cardTitle}
-        title={company}
-      >
-        <Text style={styles.optionText}>{role}</Text>
-        <Text style={styles.optionText}>{salary}</Text>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={deleteApplication(id)}
-          underlayColor="#99d9f4"
+      return (
+        <Card
+          containerStyle={styles.card}
+          titleStyle={styles.cardTitle}
+          title={company}
         >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableHighlight>
-      </Card>
-    );
-  });
+          <Text style={styles.optionText}>{role}</Text>
+          <Text style={styles.optionText}>{salary}</Text>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => deleteApplication(jobID)}
+            underlayColor="#99d9f4"
+          >
+            <Text style={styles.buttonText}>Delete</Text>
+          </TouchableHighlight>
+        </Card>
+      );
+    });
+  }
 }
 
 export default function ApplicationsScreen() {
@@ -47,13 +50,14 @@ export default function ApplicationsScreen() {
   const [applications, setApplications] = useState({});
 
   function getApplications() {
+    console.log("load");
     AsyncStorage.getAllKeys().then((key) => setKeys(key));
     AsyncStorage.multiGet(keys).then((job) => setApplications(job));
   }
 
   useEffect(() => {
     getApplications();
-  }, [keys]);
+  }, []);
 
   function check() {
     getApplications();
